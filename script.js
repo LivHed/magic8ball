0460403c482d0445ac505e740c9ce6a921657e95
LivHed <livia.hedin@gmail.com>
2020-06-29 16:42:33 +0200

    Write more clear comments, move fadein method for answer text

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     var magic8ball = {};
+    //array with possible answers for the questions asked.
     magic8ball.listOfAnswers = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes – definitely", "You may rely on it", "As I see it", "yes, Most likely", "Outlook good", "Yes, Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful" ];
     
-        // Hides the answer text when the page are loading.
+        // hides the answer text when the page are loading.
         $("#answer").hide();
         
-    //defines the method. The question function.
+    // The question function, defines the method.
     magic8ball.questionAsked = function(question) {
         
         $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
     //creates a random number
       var randomAnswer = Math.random();
         
-    //makes a random number between 0 and the number of items in your array
+    //makes a random number between 0 and the number of items in the array.
       var randomArrayItem = randomAnswer *  this.listOfAnswers.length;
     
-        // round down the number to be a whole number
+        // rounds down the number to be a whole number.
       var randomIndexItem = Math.floor(randomArrayItem);
         
-    // use that number to index a random number from the answers array
+    // uses that number to index a random number from the answers array.
       var answer =  this.listOfAnswers[randomIndexItem];
     
       $("#answer").text( answer );
+      //answer fades in
+      $("#answer").fadeIn(4000); 
         
         console.log(question);
         console.log(answer);
@@ -33,16 +36,14 @@ $(document).ready(function(){
           $("#8ball").effect( "shake" );
           
           setTimeout(function() {
-              //wait half a second before showing the prompt.
+              //waits half a second before showing the prompt.
               var question = prompt("Ask a YES/NO  Question!");
               magic8ball.questionAsked(question);
           }, 500);
           
-          //answer fades in
-          $("#answer").fadeIn(4000); 
               
       };
         
         $("#questionButton").click(clickButton);
-        console.log("does all work");
+        console.log("does it all work?");
 });
